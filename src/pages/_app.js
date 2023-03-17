f495ee65422990d6398ad04b69bf4cdf6bf1a106
Fabiano Guimarães <guimaraes_fabiano11@hotmail.com>
2023-03-16 22:30:22 -0300

    refactor:  replacement of js files for jsx

--- src/pages/_app.js
+++
@@ -1,7 +0,0 @@
-import { globalStyles } from "../styles/global";
-
-globalStyles();
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
--- src/pages/character/[slug].js
+++
@@ -1,5 +0,0 @@
-export default function PageCharacter() {
-    return (
-      <h1>Detalhes do personagem</h1>
-    )
-  }
